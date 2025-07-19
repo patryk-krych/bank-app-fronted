271ac0403b89510e765f8856311a67a955117527
1pkr1 <p.krych12@gmail.com>
2025-07-20 00:35:59 +0200

    Prepare layout for account and transfer (implementation soon)

--- src/app/dashboard/dashboard.ts
+++ src/app/dashboard/dashboard.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { Header } from './header/header';
+import { Transfer } from "./transfer/transfer";
+import { Account } from "./account/account";
 
 @Component({
   selector: 'app-dashboard',
-  imports: [Header],
+  imports: [Header, Transfer, Account],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css'
 })
