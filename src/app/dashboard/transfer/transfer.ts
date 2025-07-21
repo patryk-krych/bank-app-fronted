4fe9e7cfda278322023f4f5bc2b6f7089a14815e
1pkr1 <p.krych12@gmail.com>
2025-07-21 11:34:55 +0200

    implement transfers

--- src/app/dashboard/transfer/transfer.ts
+++ src/app/dashboard/transfer/transfer.ts
@@ -1,11 +1,45 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders  } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-transfer',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './transfer.html',
   styleUrl: './transfer.css'
 })
 export class Transfer {
+   transaction = {
+    fromAccount: '',
+    toAccount: '',
+    amount: 0,
+    currency: 'PLN',
+    date: new Date().toISOString(),
+    title: ''
+  };
+
+  http = inject(HttpClient);
+
+  submitTransfer() {
+    const token = localStorage.getItem('jwtToken');
+
+    if (!token) {
+      alert('Brak tokenu. Zaloguj się ponownie.');
+      return;
+    }
+
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${token}`
+    });
+
+    this.http.post('http://localhost:8080/api/user/transfer', this.transaction, { headers })
+      .subscribe({
+        next: () => alert('Przelew wysłany pomyślnie!'),
+        error: err => {
+          console.error(err);
+          alert('Błąd podczas wysyłania przelewu.');
+        }
+      });
+  }
 
 }
